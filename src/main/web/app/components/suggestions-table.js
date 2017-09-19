1fdfc0635c0146573eb32b684a610cb344ffce22
manos <manos@sobol>
2017-09-19 16:54:52 +0300

    backend cleanup, UI files info

---
+++ src/main/web/app/components/suggestions-table.js
@@ -0,0 +1,54 @@
+import Ember from 'ember';
+
+
+/**
+ * Provides a Datatable component for markoff record match suggestions
+ */
+export default Ember.Component.extend({
+  didInsertElement: function() {
+
+    // init datatable
+    var table = this.$('#suggestions-table').DataTable( {
+      "paging": false,
+      "searching": false,
+      "columns": [
+        { "visible": true, "type": "num"  },
+        { "visible": false },
+        { "visible": true },
+        { "visible": true, "type": "num" },
+        { "visible": false },
+        { "visible": false },
+        { "visible": false },
+        { "visible": false },
+        { "visible": false },
+        { "visible": true, "type": "num" },
+        { "visible": true, "type": "num" },
+        { "visible": false },
+        { "visible": true },
+        { "visible": true, "type": "num"  },
+        { "visible": false },
+        { "visible": false },
+        { "visible": false },
+        { "visible": false },
+        { "visible": false },
+      ]
+    });
+
+    this.$('a.toggle-vis').on( 'click', function (e) {
+      e.preventDefault();
+
+      // Get the column API object
+      let dataColumn = Ember.$(this).attr('data-column')
+      dataColumn = dataColumn.split(",");
+
+      // Toggle the visibility
+      let column;
+      for(let i=0; i < dataColumn.length; i++){
+        console.log("toggling: ", dataColumn[i]);
+        column = table.column(dataColumn[i]);
+        column.visible(!column.visible());
+      }
+
+    });
+  }
+});
